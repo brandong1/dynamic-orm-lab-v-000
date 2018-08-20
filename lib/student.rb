3cf3f6ed9e718091ed87de2dbed9c2d79919cf62
Brandon Green <brandon@brandonleeproductions.com>
2018-08-20 23:39:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student < InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(name:)
+  def self.find_by(name: name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
